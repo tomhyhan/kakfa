6c3427ee0406ad718556b275ab0640057081bce0
Tom Han <tomhyhan@gmail.com>
2021-11-04 10:17:07 -0700

    as

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -9,7 +9,7 @@ export default function EndpointAudit(props) {
 
   const getAudit = () => {
     fetch(
-      `http://http://temp6.eastus.cloudapp.azure.com:8100/stats:8110/${props.endpoint}?index=${rand_val}`
+      `http://temp6.eastus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`
     )
       .then((res) => res.json())
       .then(
